daccc5e754229819904149ca4cc087c7ec01eb06
andresgfranco <andresgfranco0@gmail.com>
2021-02-01 21:42:59 +0000

    Adding base functions and header

---
+++ 0-bubble_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+/**
+ * bubble_sort - Sorts am array of integers
+ * in ascending order using the Bubble sort
+ * algorithm
+ *
+ * @array: array of integers
+ * @size: size of the array
+ * Return: void
+**/
+void bubble_sort(int *array, size_t size)
+{
+
+}
