3a374706a75f1de60d8752e109ab1d65be90f1c7
andresgfranco <andresgfranco0@gmail.com>
2021-02-02 19:08:44 +0000

    Algorithm corrected and working as expected

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,29 +10,23 @@
 **/
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int j;
-	int tmp, swp = 0;
+	unsigned int j, i;
+	int tmp;
 
 	if (!array || size < 2)
 		return;
 
-	for (j = 0; j < size; j++)
+	for (i = 0; i < size; i++)
 	{
-		if (j + 1 < size && array[j + 1] < array[j])
+		for (j = 0; j < size; j++)
 		{
-			tmp = array[j];
-			array[j] = array[j + 1];
-			array[j + 1] = tmp;
-			swp += 1;
-			print_array(array, size);
-		}
-		if (swp == 0 && j == size - 1)
-			break;
-		if (j == size - 1)
-		{
-			j = 0;
-			swp = 0;
-			continue;
+			if (j + 1 < size && array[j + 1] < array[j])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
 		}
 	}
 }
