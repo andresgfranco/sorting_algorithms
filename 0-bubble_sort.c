45a14ecf71e41c377e0123b469ff7d1dff4ecc8a
andresgfranco <andresgfranco0@gmail.com>
2021-02-02 16:01:00 +0000

    Bubble sort algorithm 90% ready

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,5 +10,29 @@
 **/
 void bubble_sort(int *array, size_t size)
 {
+	unsigned int j;
+	int tmp, swp = 0;
 
+	if (!array || size < 2)
+		return;
+
+	for (j = 0; j < size; j++)
+	{
+		if (j + 1 < size && array[j + 1] < array[j])
+		{
+			tmp = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = tmp;
+			swp += 1;
+			print_array(array, size);
+		}
+		if (swp == 0 && j == size - 1)
+			break;
+		if (j == size - 1)
+		{
+			j = 0;
+			swp = 0;
+			continue;
+		}
+	}
 }
