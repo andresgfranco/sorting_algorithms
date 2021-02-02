fab411054f79556d5a92d6d9d03c63e6e4cbef0e
andresgfranco <andresgfranco0@gmail.com>
2021-02-02 19:20:08 +0000

    Correction on first loop

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -16,7 +16,7 @@ void bubble_sort(int *array, size_t size)
 	if (!array || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
