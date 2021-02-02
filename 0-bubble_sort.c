e1434735bc59b55470cb4622796dfec3ae01aa3f
andresgfranco <andresgfranco0@gmail.com>
2021-02-02 19:21:15 +0000

    Correction on second loop

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,7 +18,7 @@ void bubble_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (j + 1 < size && array[j + 1] < array[j])
 			{
